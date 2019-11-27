1c695f71777a06b081d1e767b16097162b8a1cfe
Anant00 <anantpundir02@gmail.com>
2019-11-27 09:50:52 +0530

    inject retrofit and network repo

--- app/src/main/java/com/app/sportsnews/di/AppModule.kt
+++ app/src/main/java/com/app/sportsnews/di/AppModule.kt
@@ -1,6 +1,51 @@
 package com.app.sportsnews.di
 
+import com.app.sportsnews.api.apiservice.Api
+import com.app.sportsnews.repository.NetworkRepository
+import com.app.sportsnews.utils.Constants
 import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 @Module
-class AppModule
+class AppModule {
+
+    @Module
+    companion object {
+        @Singleton
+        @Provides
+        @JvmStatic
+        fun provideApiService(client: OkHttpClient): Api {
+            return Retrofit.Builder()
+                .baseUrl(Constants.BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+                .create(Api::class.java)
+        }
+
+        @Singleton
+        @Provides
+        @JvmStatic
+        fun provideOkHttpClient(): OkHttpClient {
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+            return OkHttpClient.Builder().addInterceptor(interceptor).build()
+        }
+
+        @Singleton
+        @Provides
+        @JvmStatic
+        fun provideNetworkRepo(api: Api): NetworkRepository {
+            return NetworkRepository(
+                api
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/app/sportsnews/repository/NetworkRepository.kt
@@ -0,0 +1,6 @@
+package com.app.sportsnews.repository
+
+import com.app.sportsnews.api.apiservice.Api
+import javax.inject.Inject
+
+class NetworkRepository @Inject constructor(val api: Api)

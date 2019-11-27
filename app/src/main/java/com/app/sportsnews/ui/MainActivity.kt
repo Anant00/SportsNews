4bed5957bbf97c0335f2d2e89106d9f01f1edcb3
Anant00 <anantpundir02@gmail.com>
2019-11-27 10:55:20 +0530

    fetch static sport

--- app/src/main/java/com/app/sportsnews/ui/MainActivity.kt
+++ app/src/main/java/com/app/sportsnews/ui/MainActivity.kt
@@ -1,12 +1,50 @@
 package com.app.sportsnews.ui
 
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.app.sportsnews.R
+import com.app.sportsnews.di.AppViewModelFactory
+import com.app.sportsnews.utils.Status
+import com.app.sportsnews.utils.showLog
+import com.app.sportsnews.viewmodels.MainViewModel
 import dagger.android.support.DaggerAppCompatActivity
+import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity() {
+    @Inject
+    lateinit var viewModelFactory: AppViewModelFactory
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setViewModel()
+        setSearchQuery()
+        setData()
+    }
+
+    private fun setViewModel() {
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(MainViewModel::class.java)
+    }
+
+    private fun setData() {
+        viewModel.results.observe(this, Observer {
+
+            when (it.status) {
+                Status.SUCCESS -> {
+                    showLog("success, ${it.data?.hits?.size}")
+                }
+                Status.ERROR -> {
+                    showLog("error ${it.message}")
+                }
+                Status.LOADING -> {
+                    showLog("Loading")
+                }
+            }
+        })
+    }
+
+    private fun setSearchQuery() {
+        viewModel.setQuery("baseball")
     }
 }
---
+++ app/src/main/java/com/app/sportsnews/viewmodels/MainViewModel.kt
@@ -0,0 +1,30 @@
+package com.app.sportsnews.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import com.app.sportsnews.api.apimodels.SearchHits
+import com.app.sportsnews.repository.NetworkRepository
+import com.app.sportsnews.utils.Resource
+import java.util.Locale
+import javax.inject.Inject
+
+class MainViewModel
+@Inject constructor(repo: NetworkRepository) : ViewModel() {
+
+    private val _query = MutableLiveData<String>()
+
+    val results: LiveData<Resource<SearchHits>> = Transformations
+        .switchMap(_query) { search ->
+            repo.fetchSearchResult(search, "0")
+        }
+
+    fun setQuery(originalInput: String) {
+        val input = originalInput.toLowerCase(Locale.getDefault()).trim()
+        if (input == _query.value) {
+            return
+        }
+        _query.value = input
+    }
+}
